729a5bb2ce8dd67c42e83b7cdc4fc45ba39d32a8
KohnoA <san.kohno@yandex.ru>
2024-03-07 16:22:08 +0300

    feat: update eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import { useState } from 'react';
+
 import ReactLogo from './assets/react.svg';
+
 import './App.css';
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev">
+          React Logo
           <ReactLogo width={50} height={50} />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <button type="button" onClick={() => setCount((prev) => prev + 1)}>
+          count is {count}
+        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -22,6 +27,6 @@ function App() {
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
-}
+};
 
 export default App;
