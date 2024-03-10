9a38b4be8561d630f3ededf9c676d79b6216cc4a
KohnoA <san.kohno@yandex.ru>
2024-03-10 21:16:20 +0300

    feat: update icons object

--- src/constants/icons.ts
+++ src/constants/icons.ts
@@ -1,7 +1,9 @@
+import ArrowDownIcon from '@/assets/icons/arrow-down.svg';
 import GoogleIcon from '@/assets/icons/google-icon.svg';
 import TwitterIcon from '@/assets/icons/twitter-logo.svg';
 
 export const ICONS = {
   TwitterIcon,
   GoogleIcon,
+  ArrowDownIcon,
 };
