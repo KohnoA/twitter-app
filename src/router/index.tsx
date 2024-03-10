bda430f76d1fb0f4da6b2cc6af5ea368964cb4a3
KohnoA <san.kohno@yandex.ru>
2024-03-10 17:38:13 +0300

    feat: update app router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,13 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 
 import { AppRoutes } from '@/constants';
-import { Login, SignUp } from '@/pages';
+import { LoginPage, SignUpEmailPage, SignUpPage } from '@/pages';
 
 export const AppRouter = () => (
   <Routes>
     <Route path={AppRoutes.ROOT} element={<Navigate to={AppRoutes.SIGN_UP} replace />} />
-    <Route path={AppRoutes.LOGIN} element={<Login />} />
-    <Route path={AppRoutes.SIGN_UP} element={<SignUp />} />
+    <Route path={AppRoutes.LOGIN} element={<LoginPage />} />
+    <Route path={AppRoutes.SIGN_UP} element={<SignUpPage />} />
+    <Route path={AppRoutes.SIGN_UP_EMAIL} element={<SignUpEmailPage />} />
   </Routes>
 );
