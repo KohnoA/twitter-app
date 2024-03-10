90ee5d46193e4e90393a70e41a1773434a9bfdeb
KohnoA <san.kohno@yandex.ru>
2024-03-10 17:32:25 +0300

    feat: update app constants

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,5 +1,20 @@
 export enum AppRoutes {
   ROOT = '/',
   SIGN_UP = '/signup',
+  SIGN_UP_EMAIL = '/signup-email',
   LOGIN = '/login',
+}
+
+export enum AppThemes {
+  LIGHT = 'light',
+  DARK = 'dark',
+}
+
+export enum AppColors {
+  WHITE = '#FFFFFF',
+  BLACK = '#000000',
+  BLUE_LIGHT = '#1DA1F2',
+  BLUE_DARK = '#1D9BF0',
+  GREY_LIGHT = '#EFF3F4',
+  GREY_DARK = '#B3B8BB',
 }
