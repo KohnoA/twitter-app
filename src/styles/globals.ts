a7f80c46933782726ccd87c37a7da9e0950cd53e
KohnoA <san.kohno@yandex.ru>
2024-03-10 17:33:41 +0300

    feat: created global styles

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -1,9 +1,34 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
   body {
-    font-family: 'Roboto', 'Arial', sans-serif;
-    font-size: 18px;
-    color: black;
+    min-width: ${({ theme }) => theme.breakpoints.mobileS};
+
+    margin: 0;
+    padding: 0;
+  
+    font-family: ${({ theme }) => theme.fontFamily};
+    font-size: ${({ theme }) => theme.fontSizes.xl};
+    color: ${({ theme }) => theme.colors.text};
+
+    background-color: ${({ theme }) => theme.colors.bgPrimary};
+  }
+
+  a {
+    color: ${({ theme }) => theme.colors.text};
+    text-decoration: none;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+
+    list-style: none;
   }
 `;
