2d544cffd59d4b17835a0dcabdfea364d8fc724a
KohnoA <san.kohno@yandex.ru>
2024-03-11 01:21:29 +0300

    feat: added error messages constants

---
+++ src/constants/errors.ts
@@ -0,0 +1,6 @@
+export enum Errors {
+  REQUIRED = 'This field is required',
+  NAME = 'The name must begin with a capital letter and contain from 3 to 30 characters',
+  PHONE = 'Invalid phone number, required "+375 (29) 111 22 33"',
+  EMAIL = 'Invalid email value',
+}
