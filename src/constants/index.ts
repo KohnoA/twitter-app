a820e627d925bb80f21fcf77feb2ec5f678803e1
KohnoA <san.kohno@yandex.ru>
2024-03-11 01:21:59 +0300

    feat: added validation regexes

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,8 @@
 export * from './app';
+export * from './errors';
 export { FOOTER_LINKS } from './footerLinks';
 export * from './icons';
+export * from './regexes';
 export * from './signup';
 
 export const UNASSIGNED_LINK_VALUE = '#';
