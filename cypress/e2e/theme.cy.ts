e4561b1c31e893f6935c653037cd61de00eeea20
KohnoA <san.kohno@yandex.ru>
2024-03-21 21:27:27 +0300

    feat: added cypress auth commands

--- cypress/e2e/theme.cy.ts
+++ cypress/e2e/theme.cy.ts
@@ -1,22 +1,12 @@
-import testUser from '../fixtures/testUser.json';
-
 describe('Theme change testing', () => {
   before(() => {
-    const { email, password } = testUser;
-
-    cy.visit('/login');
-    cy.get('[data-testid=login-email-input]').type(email);
-    cy.get('[data-testid=login-password-input]').type(password);
-    cy.get('[data-testid=login-submit-button]')
-      .click()
-      .then(() => {
-        cy.url().should('include', '/home');
-      });
+    cy.logout();
+    cy.login();
   });
 
   it('', () => {});
 
   it('save theme value after page reload', () => {});
 
-  // after(() => );
+  after(() => cy.logout());
 });
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,62 +1,21 @@
 /// <reference types="cypress" />
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
 
-// import { signOut, signIn, signUp } from '../../src/services/authentication';
-// import testUser from '../fixtures/testUser.json';
+Cypress.Commands.add('logout', () => {
+  indexedDB.deleteDatabase('firebaseLocalStorageDb');
+});
 
-// Cypress.Commands.add('logout', () => {
-//   signOut();
-// });
+Cypress.Commands.add('login', () => {
+  cy.fixture('testUser').then((testUser) => {
+    cy.visit('/login');
+    cy.get('[data-testid=login-email-input]').type(testUser.email);
+    cy.get('[data-testid=login-password-input]').type(testUser.password);
+    cy.get('[data-testid=login-submit-button]').click();
+  });
+});
 
-// Cypress.Commands.add('login', (email = testUser.email, password = testUser.password) => {
-//   signIn(email, password);
-// });
-
-// Cypress.Commands.add('signup', (userData = testUser) => {
-//   signUp(userData);
-// });
-
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       logout(): Chainable<void>;
-//       login(email: string, password: string): Chainable<void>;
-//       signup(userData: typeof testUser): Chainable<void>;
-//     }
-//   }
-// }
+declare namespace Cypress {
+  interface Chainable {
+    logout(): void;
+    login(): void;
+  }
+}
