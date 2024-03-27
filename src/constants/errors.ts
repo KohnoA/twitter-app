77add623050f873694e6cd7c6d18f0b3b67502fd
KohnoA <san.kohno@yandex.ru>
2024-03-27 22:50:56 +0300

    fix: added more accurate password error message

--- src/constants/errors.ts
+++ src/constants/errors.ts
@@ -3,7 +3,7 @@ export enum Errors {
   NAME = 'The name must begin with a capital letter and contain from 3 to 30 characters',
   PHONE = 'Invalid phone number, required format "+375 (29) 111-22-33"',
   EMAIL = 'Invalid email value',
-  PASSWORD = 'The password must contain a minimum of 8 characters, one capital letter and one number',
+  PASSWORD = 'The password must consist of numbers and upper and lower case Latin letters',
   PASSWORD_CONFIRM = 'Passwords must match',
   GENERAL_ERROR = 'Something went wrong, try again later',
   USER_EXIST = 'A user with this email address is already registered',
